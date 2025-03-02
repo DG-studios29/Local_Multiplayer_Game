42a5b5bd0541fb99f478bc9b74286f3725ab4379
Michael-Boateng <131008187+Michael-Boat-2@users.noreply.github.com>
2025-03-02 13:29:22 +0200

    Set up for auto-shooting items

---
+++ Local_Mulitplayer_Game/Assets/Scripts/AutoAttacks/AutoAttack.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AutoAttack : MonoBehaviour
+{
+    public string playerTag = "Player";  //will latch onto this object's transform
+
+    public GameObject playerTestObj;
+    public Transform playerObjOrigin;
+
+    public List<ItemObject> itemHolder;
+    public int itemSize;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerTestObj = GameObject.FindGameObjectWithTag(playerTag);
+
+        if (playerTestObj != null)
+        {
+            playerObjOrigin = playerTestObj.transform;
+            transform.SetParent(playerObjOrigin);
+
+
+            this.transform.position = playerObjOrigin.position;
+        }
+
+        ItemHolder();  // initialising the item slots
+        
+    }
+
+    public void ItemHolder()
+    {
+        itemHolder = new List<ItemObject>(itemSize);
+
+        for(int i = 0; i < itemSize; i++)
+        {
+            itemHolder.Add(new ItemObject());
+        }
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = playerObjOrigin.position;
+    }
+}
